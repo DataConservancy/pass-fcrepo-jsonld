e2e2cdb1f7e57d9c5f9774e86a31746bc2177994
Mark Patton <mark.patton@gmail.com>
2022-07-26 15:39:26 -0400

    Ensure that graph is returned when inbound references prefer header is set.

--- jsonld-addon-integration/src/test/java/org/dataconservancy/fcrepo/jsonld/integration/CompactionIT.java
+++ jsonld-addon-integration/src/test/java/org/dataconservancy/fcrepo/jsonld/integration/CompactionIT.java
@@ -18,13 +18,19 @@ package org.dataconservancy.fcrepo.jsonld.integration;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.dataconservancy.fcrepo.jsonld.test.JsonldTestUtil.assertCompact;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -37,6 +43,8 @@ import org.apache.http.util.EntityUtils;
 import org.fcrepo.client.FcrepoClient;
 import org.fcrepo.client.FcrepoClient.FcrepoClientBuilder;
 import org.fcrepo.client.FcrepoResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
 
 /**
@@ -120,6 +128,75 @@ public class CompactionIT implements FcrepoIT {
 
             final String body = IOUtils.toString(response.getBody(), UTF_8);
             assertCompact(body);
+
+            assertNotNull(response.getHeaderValue("X-CREATED"));
+            assertNotNull(response.getHeaderValue("X-MODIFIED"));
+        }
+    }
+
+    @Test
+    // The pass-java-client has a getIncoming method which expects a @graph returned
+    // when inbound references are specified in the Prefer header.
+    public void testInboundPrefer() throws Exception {
+        final FcrepoClient client = new FcrepoClientBuilder().throwExceptionOnFailure().build();
+
+        final URI childJsonldResource1 = attempt(60, () -> {
+            try (FcrepoResponse response = client
+                .post(URI.create(fcrepoBaseURI))
+                .body(this.getClass().getResourceAsStream("/compact-uri.json"), "application/ld+json")
+                .perform()) {
+                return response.getLocation();
+            }
+        });
+
+        final URI childJsonldResource2 = attempt(60, () -> {
+            try (FcrepoResponse response = client
+                .post(URI.create(fcrepoBaseURI))
+                .body(this.getClass().getResourceAsStream("/compact-uri.json"), "application/ld+json")
+                .perform()) {
+                return response.getLocation();
+            }
+        });
+
+        final URI jsonldResource = attempt(60, () -> {
+            String json = "{\n"
+                    + "  \"@context\": \"http://example.org/farm\",\n"
+                    + "  \"@id\": \"\",\n"
+                    + "  \"@type\": \"Cow\",\n"
+                    + "  \"calves\": [\"" + childJsonldResource1 + "\", \"" + childJsonldResource2 + "\"],\n"
+                    + "  \"healthy\": true,\n"
+                    + "  \"name\": \"bob\",\n"
+                    + "  \"weight\": 10000\n"
+                    + "}";
+
+            try (FcrepoResponse response = client
+                .post(URI.create(fcrepoBaseURI))
+                .body(new ByteArrayInputStream(json.getBytes(Charset.forName("UTF-8"))), "application/ld+json")
+                .perform()) {
+                return response.getLocation();
+            }
+        });
+
+        try (FcrepoResponse response = client
+            .get(childJsonldResource1)
+            .accept("application/ld+json")
+            .preferRepresentation(Arrays.asList(URI.create("http://fedora.info/definitions/v4/repository#InboundReferences")), Arrays.asList())
+            .perform()) {
+
+            final String body = IOUtils.toString(response.getBody(), UTF_8);
+
+            JSONObject o = new JSONObject(body);
+            assertTrue(o.has("@graph"));
+            JSONArray grapharr = o.getJSONArray("@graph");
+            assertEquals(2, grapharr.length());
+            JSONObject o1 = grapharr.getJSONObject(0);
+            JSONObject o2 = grapharr.getJSONObject(1);
+            Set<String> ids = new HashSet<>();
+            ids.add(o1.getString("@id"));
+            ids.add(o2.getString("@id"));
+
+            assertTrue(ids.contains(childJsonldResource1.toString()));
+            assertTrue(ids.contains(jsonldResource.toString()));
 
             assertNotNull(response.getHeaderValue("X-CREATED"));
             assertNotNull(response.getHeaderValue("X-MODIFIED"));
